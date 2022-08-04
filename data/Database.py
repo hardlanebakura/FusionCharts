98d95bfb0155cd8ce9f9f1002e94c488b8530d18
= <hardlane2000@gmail.com>
2022-08-04 19:48:37 +0200

    merge tables

--- data/Database.py
+++ data/Database.py
@@ -1,6 +1,7 @@
 import json
 import mysql.connector
 from mysql.connector.cursor import MySQLCursorPrepared
+from log_config import logging
 
 class Database(object):
 
@@ -26,20 +27,17 @@ class Database(object):
         file = open("./json/all_continents.json", "r")
         data = json.loads(file.read())
         countries = [item[1] for item in self.cursor.fetchall()]
-        print(countries)
-        print(data[0].items())
         self.cursor.execute("SELECT a.Country, a.`index` AS A, b.`index` AS B, c.`index` AS C, d.`index` FROM fragile_states_indexes as a INNER JOIN factionalized_elites_indexes as b ON a.Country = b.Country INNER JOIN group_grievances_indexes as c ON a.Country = c.Country INNER JOIN military_spendings_indexes as d ON a.Country = d.Country ORDER BY (A + B + C) DESC")
         rankings = self.cursor.fetchall()
         for item in rankings:
             rankings[rankings.index(item)] = list(item)
             rankings[rankings.index(list(item))][-1] = [country_data["continent"] for country_data in data if country_data["country"] == rankings[rankings.index(list(item))][0]][0]
-        print([item for item in rankings])
-        return self.cursor.fetchall()
+        return rankings
 
     def __del__(self):
         self.con.close()
 
 db = Database()
 d = db.merge_tables()
-#for row in d:
-    #print(row)
+for row in d:
+    logging.info(row)
